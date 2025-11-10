63dbfdf3deb8c099945e3f9ed83fc032bec53c66
David Carlos <davidac4@live.com>
2025-11-10 14:49:52 -0300

    fix(refactor): additional dummy data scenarios and change date conversion logic

--- common/df.py
+++ common/df.py
@@ -1,6 +1,27 @@
 import pandas as pd
 
-# caminho_arquivo = "./gastos.csv"
-caminho_arquivo = "./dummy_data.csv"
+caminho_arquivo = "./dummy_data1.csv"
+"""
+Cenário sucesso:
+- Número de colunas igual ao esperado;
+- Colunas esperadas estão presentes;
+- Dtypes de cada coluna são adequados.
+"""
+
+# caminho_arquivo = "./dummy_data2.csv"
+"""
+Cenário erro:
+- Número de colunas diferente do esperado;
+- Colunas Gasto não presente e Fonte Pagamento com nome errado;
+- Dtypes das colunas válidas são adequados.
+"""
+
+# caminho_arquivo = "./dummy_data3.csv"
+"""
+Cenário erro:
+- Número de colunas igual ao esperado;
+- Colunas esperadas estão presentes;
+- Dtype inválido da coluna Fonte Pagamento.
+"""
 
 df = pd.read_csv(caminho_arquivo)
